43d9695ab239a001e6d929f8b5aa2c8e1c2d711e
vinaytech23 <vinaytech23@gmail.com>
2020-05-15 09:05:30 +0530

    added material to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
