8b98355730edb7dc1fe5dbdcab9d4b709ee63008
vinaytech23 <vinaytech23@gmail.com>
2020-05-15 09:18:45 +0530

    created material module for adding all the material content to module

---
+++ src/app/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
+
+  ]
+})
+export class MaterialModule { }
